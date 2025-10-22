8984f85376a2da6642126c3aab1ec225a85674ba
Stefan Balanica <159913350+StefanBalanica@users.noreply.github.com>
2025-10-22 08:59:52 +0300

    .

--- src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import {Routes} from '@angular/router'
-
-export const routes: Routes = [
-  {path: '', redirectTo: 'enjoy', pathMatch: 'full'},
-  {
-    path: 'enjoy',
-    loadComponent: () => import('./features/enjoy/enjoy.component').then(c => c.EnjoyComponent)
-  }
-];
